baaad52b948e524407d4bec851c185313d9b71d1
asiya.batool <asiya.batool987@gmail.com>
2022-11-24 10:22:36 +0500

    1st commit in FE-6

--- src/Components/Loading/index.js
+++ src/Components/Loading/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import '../../Assets/styles/generalstyles.css';
-import LoadingImg from '../../Assets/images/loadingCricle.gif';
 
-const LoadingScreen = (props) => {
+const LoadingScreen =(props)=>{
     return (
         <div className="loadingSpinnerDiv">
-            <img className="loadingSpinner" src={LoadingImg} alt="loading..." />
-            <div className="loadingMsg">{props.msg}</div> {/* props.msg || Loading.. if props is empty*/}
+            <img className="loadingSpinner" src={require('../../Assets/images/loading.gif')} alt="loading..."/>   
+            <div className="loadingMsg">{props.msg }</div> {/* props.msg || Loading.. if props is empty*/}
         </div>
     )
 }
-LoadingScreen.defaultProps = {
-    msg: 'Loading..'
+LoadingScreen.defaultProps={
+    msg:'Loading..'
 }
 export default LoadingScreen;
